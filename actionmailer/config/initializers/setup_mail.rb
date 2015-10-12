f0bb4f1ba846d017bd075e004323b260e3c32264
munyite-megzs <margaret.sitati1@gmail.com>
2015-10-12 12:24:50 +0300

    populated data to the action mailer

---
+++ actionmailer/config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "margaret.sitati1@gmail.com",
+  :password             => "jkc-bo9-0024/2011",
+  :authentication       => :plain,
+  :enable_starttls_auto => true
+}
+
